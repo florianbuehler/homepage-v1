b9f4117ec31675fe6d922f631a45236bee93f5a3
Florian Bühler <florian-buehler@outlook.com>
2021-03-28 16:16:32 +0200

    Add content for hero header.

--- src/components/sections/heroHeader/index.tsx
+++ src/components/sections/heroHeader/index.tsx
@@ -4,10 +4,27 @@ import Section from '../../atoms/section'
 // styles
 import classes from './styles.module.scss'
 
+// images
+import CodeReview from 'assets/svgs/undraw_code_review.svg'
+
 const HeroHeader: React.FC = (): React.ReactElement => {
   return (
     <Section id="heroHeader" className={classes.container}>
-      hero header
+      <div className={classes.top} />
+      <div className={classes.content}>
+        <div className={classes.text}>
+          <h1>I am a Full Stack Developer</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ad aperiam deleniti doloremque dolores fuga
+            impedit nesciunt placeat, qui totam? Adipisci assumenda itaque minus modi nemo obcaecati perspiciatis quod
+            sunt!
+          </p>
+        </div>
+        <div className={classes.img}>
+          <CodeReview />
+        </div>
+      </div>
+      <div className={classes.bottom} />
     </Section>
   )
 }
