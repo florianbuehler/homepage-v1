4fd6033e3136d8877e1678611885c3d8deb821e5
Florian Bühler <florian-buehler@outlook.com>
2021-04-05 14:05:37 +0200

    Add newest version of the resume.

--- src/components/sections/about/index.tsx
+++ src/components/sections/about/index.tsx
@@ -17,13 +17,15 @@ const About: React.FC = (): React.ReactElement => {
       <p>
         I&apos;m a passionate full stack developer with a strong hands-on mentality and over 3 years of experience
         building scalable web applications in agile teams from front to back - for both prototypes and existing systems.
-        My tasks have been including requirement gathering, solution design, coordination with business owners,
-        implementation, testing and documentation.
+        I possess strong analytical, methodical and problem-solving skills sharpened during five years of mathematical
+        education and over four years of work as a consultant and senior consultant for multiple clients.
       </p>
       <p>
         Since March 2017 I am working as a consultant and later as a senior consultant for d-fine. D-fine is a leading
-        European consulting firm with scientifically mindend employees, which provides innovative, customized and
-        futureproof software solutions for complex challenges for it&apos;s clients.
+        European consulting firm with scientifically minded employees, which provides innovative, customized and
+        futureproof software solutions for complex challenges for its clients. On a day to day basis I am engaging with
+        requirement gathering, solution design, coordination with business owners, implementation, testing and
+        documentation.
       </p>
       <div className={classes.additionalInformation}>
         <ul>
